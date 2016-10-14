5b4475b55d876b23e7d688a363381b1894b70835
Jakob Mass <jaks@ut.ee>
2016-10-14 13:29:47 +0300

    Removed fields

--- app/src/test/java/ee/ut/cs/mc/and/testingapplication1311/ExampleUnitTest.java
+++ app/src/test/java/ee/ut/cs/mc/and/testingapplication1311/ExampleUnitTest.java
@@ -5,13 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
 public class ExampleUnitTest {
-    private ShoppingBasket basket;
-    private ShoppingItem item1;
-    private ShoppingItem item2;
 
     @Test
     public void addition_isCorrect() throws Exception {
